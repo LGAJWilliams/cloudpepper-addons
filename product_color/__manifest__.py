6bcbb0d36847410b9a80b26729a87df36f232016
LGAJWilliams <justin@littleglassart.co>
2024-11-23 16:55:44 -0700

    Update __manifest__.py

--- product_color/__manifest__.py
+++ product_color/__manifest__.py
@@ -1,10 +1,10 @@
 {
-    'name': 'Product Color Customization',
-    'description': 'Adds a color field to the product template model.',
-    'version': '18.0.1.0.0',
-    'depends': ['mis_color_picker'],  # Depends on the color picker addon
-    'data': [
-        'models.py',
+    "name": "Product Color Customization",
+    "description": "Adds a color field to the product template model.",
+    "version": "18.0.1.0.0",
+    "depends": ["mis_color_picker"],
+    "data": [
+        "models.py"  # Use "data" for Python models
     ],
     # ... you can add other metadata like author, website, etc. ...
 }
