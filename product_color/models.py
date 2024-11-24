c9dd18693440fb6a7cf2a3fdb2d231a333574380
LGAJWilliams <justin@littleglassart.co>
2024-11-23 18:28:19 -0700

    Update models.py

--- product_color/models.py
+++ product_color/models.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    x_glass_colors = fields.Char(string='Glass Colors', store=True)
+    x_studio_glass_colors = fields.Char(string='Glass Colors', store=True)
