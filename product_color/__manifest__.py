d95bedc23aacb17a516f99a55d6d52e64d53daec
LGAJWilliams <justin@littleglassart.co>
2024-11-23 17:59:06 -0700

    Update __manifest__.py

--- product_color/__manifest__.py
+++ product_color/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Product Color Customization",
     "description": "Adds a color field to the product template model.",
     "version": "18.0.1.0.0",
-    "depends": ["mis_color_picker"],
+    "depends": ["product", "mis_color_picker"],
     "data": [
         "views/product_template_views.xml"  # The correct location of the view file
     ],
