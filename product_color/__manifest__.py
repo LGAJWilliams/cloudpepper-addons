221938d2b540869e67fdae7567802551d689b14c
LGAJWilliams <justin@littleglassart.co>
2024-11-23 17:36:31 -0700

    Update __manifest__.py

--- product_color/__manifest__.py
+++ product_color/__manifest__.py
@@ -4,7 +4,7 @@
     "version": "18.0.1.0.0",
     "depends": ["mis_color_picker"],
     "data": [
-        "models.py"  # Use "data" for Python models
+        "views/product_template_views.xml"  # The correct location of the view file
     ],
     # ... you can add other metadata like author, website, etc. ...
 }
